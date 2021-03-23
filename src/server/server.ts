86b3c348bb1aedf833c1682b6220817320541949
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-23 18:00:00 -0500

    added app dot get for user to be rerouted to home page if they hit a path that is not there

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,5 @@
 import * as express from 'express';
+import * as path from "path";
 import apiRouter from './routes';
 
 const app = express();
@@ -6,6 +7,8 @@ const app = express();
 app.use(express.static('public'));
 app.use(express.json()); // this is where we will make POST request to - it must be above the apiRouter
 app.use(apiRouter);
+// if the route doesn't match then send to home page -- line below
+app.get("*", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server listening on port: ${port}`));
