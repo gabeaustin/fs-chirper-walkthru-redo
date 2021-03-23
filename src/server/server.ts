c8eac9f4972b874385a37f403b7fe705d6eabe17
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-23 18:07:17 -0500

    added dotenv to hide environment variables and added config folder

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,11 +1,14 @@
 import * as express from 'express';
 import * as path from "path";
+import * as morgan from "morgan";
 import apiRouter from './routes';
 
 const app = express();
 
-app.use(express.static('public'));
+app.use(express.static('public')); // ?? look up why PUBLIC
 app.use(express.json()); // this is where we will make POST request to - it must be above the apiRouter
+// this will log the api routes - using Express middleware
+app.use(morgan("dev"));
 app.use(apiRouter);
 // if the route doesn't match then send to home page -- line below
 app.get("*", (req, res) => res.sendFile(path.join(__dirname, "../public/index.html")));
