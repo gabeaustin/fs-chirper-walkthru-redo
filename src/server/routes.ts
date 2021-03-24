b5c0d3c808ceee3470852887faf158ee1c30f4c2
Gabriel Austin <gabriel.austin2019@gmail.com>
2021-03-23 22:55:33 -0500

    clean up code

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -1,9 +1,9 @@
-import * as express from 'express';
+import * as express from "express";
 
 const router = express.Router();
 
-router.get('/api/hello', (req, res, next) => {
-    res.json('World');
+router.get("/hello", (req, res, next) => {
+    res.json("World");
 });
 
 export default router;
